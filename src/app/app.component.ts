b88b3b52461d260a4265144c65a89a11571255a4
Belén <mgiannone@frro.utn.edu.ar>
2024-09-13 09:46:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  Inicial = 1000;
+  coleccion:any = [1, 2, 3, 4, 5, 6];
+
+  agregarElemento(){
+    this.coleccion.push('X');
+  }
 }
